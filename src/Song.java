3beeeb1ff402abcc60f05ac0c520d98cf3486002
Ani Ordyan <ordyanani@gmail.com>
2024-03-26 11:40:07 +0400

    Made changes in Song class(need a review) Added MusicPlayer class

--- src/Song.java
+++ src/Song.java
@@ -1,19 +1,43 @@
+import com.mpatric.mp3agic.Mp3File;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+
+import java.io.File;
+
 public class Song {
     private String songName;
     private int songDuration;
     private People[] singers;
+    private String filePath;
 
     //constructor
 
-    public Song(String songName, int songDuration, People[] singers ){
+    public Song(String songName, int songDuration, People[] singers, String filePath ){
         this.songName = songName;
         this.songDuration = songDuration;
         this.setSingers(singers);
+        this.filePath = filePath;
+        try{
+            // uses jaudiotagger lib to create an audiofile obj to read mp3
+            AudioFile audioFile = AudioFileIO.read(new File(filePath));
+
+            Tag tag = audioFile.getTag();
+            if(tag != null){
+                songName = tag.getFirst(FieldKey.TITLE);
+                //fix song Artist of People array
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     public Song(Song original){ //copy constructor
         this.songName = original.songName;
         this.songDuration = original.songDuration;
         this.singers = original.getSingers();
+        this.filePath = original.filePath;
     }
 
     public String getSongName(){
@@ -50,5 +74,9 @@ public class Song {
         this.singers = temp;
     }
 
+
+    public String getFilePath(){
+        return this.filePath;
+    }
 
 }
