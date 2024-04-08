34b0bda38a536ded76b196d5f65ae8bbbaf6f4ed
Karine <karine_grigoryan@edu.aua.am>
2024-04-08 20:33:30 +0400

    simple things in Motorbike class

---
+++ src/Vehicles/Motorbike.java
@@ -0,0 +1,32 @@
+package Vehicles;
+
+import Operations.Rentable;
+
+public class Motorbike extends Vehicle implements Rentable {
+    public enum MotorbikeBody{SCOOTER, STANDARD, SPORTBIKE, CRUISER};
+    private boolean available = true;
+    private MotorbikeBody style;
+
+    public Motorbike(int id,
+                     String brand,
+                     String model,
+                     int yearOfProduction,
+                     int ratePerDay,
+                     MotorbikeBody style){
+        super(id,brand,model,yearOfProduction,ratePerDay);
+        this.style = style;
+    }
+    public MotorbikeBody getStyle(){return this.style;}
+
+    public void setStyle(MotorbikeBody style){this.style = style;}
+
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Motorbike body: " + style);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
+}
