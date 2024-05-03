f699c1dcd6c28813d5991abbcbd64151058f0d2b
Ani Ordyan <ordyanani@gmail.com>
2024-05-03 18:04:53 +0400

    create new playlist button is now working

--- src/am/aua/ui/AudioPlayerUi.java
+++ src/am/aua/ui/AudioPlayerUi.java
@@ -31,6 +31,7 @@ public class AudioPlayerUi extends JFrame {
      static JPanel audiofileListPanel;
     private static JPanel mainPanel;
     private static JPanel welcomePanel;
+    static JPanel controlPanel;
     static AudioFile selectedAudio;
     Song selectedSong;
     boolean isPaused;
@@ -57,13 +58,13 @@ public class AudioPlayerUi extends JFrame {
 
 
         // Control panel with buttons
-        JPanel controlPanel = new JPanel();
+        controlPanel = new JPanel();
         controlPanel.setPreferredSize(new Dimension(SIDE_WIDTH, HEIGHT));
         controlPanel.setBackground(Color.LIGHT_GRAY);
-        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.PAGE_AXIS));
 
         controlPanel.setSize(300, 150);
-        controlPanel.setLayout(new java.awt.FlowLayout());
+       // controlPanel.setLayout(new java.awt.FlowLayout());
         controlPanel.setVisible(true);
 
 
@@ -73,6 +74,7 @@ public class AudioPlayerUi extends JFrame {
         JLabel rockPlylistLabel = createClickableLabel("Rock");
 
         JPanel dropDownSongs = new JPanel();
+        dropDownSongs.setLayout(new BoxLayout(dropDownSongs, BoxLayout.Y_AXIS));
         dropDownSongs.setPreferredSize(new Dimension(80,70));
         dropDownSongs.setBackground(Color.LIGHT_GRAY);
         dropDownSongs.setVisible(false);
@@ -89,6 +91,7 @@ public class AudioPlayerUi extends JFrame {
             }
         });
         JPanel dropDownPodcasts = new JPanel();
+        dropDownPodcasts.setLayout(new BoxLayout(dropDownPodcasts, BoxLayout.Y_AXIS));
         dropDownPodcasts.setPreferredSize(new Dimension(80,60));
         dropDownPodcasts.setBackground(Color.LIGHT_GRAY);
         dropDownPodcasts.setVisible(false);
@@ -185,7 +188,7 @@ public class AudioPlayerUi extends JFrame {
     }
 
 
-    private JLabel createClickableLabel(String labelText) {
+    static JLabel createClickableLabel(String labelText) {
         JLabel label = new JLabel(labelText);
         label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         return label;
